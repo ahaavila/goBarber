7ded7cea32a72d7f6aeb5ffcbceaa66a7d47612e
Augusto <ahaavila@gmail.com>
2020-05-09 11:03:40 -0300

    Estilização e funcionalidades
    Adicionado página de cadastro, unform e usabilidades do input.

---
+++ gobarber-web/src/components/Input/index.tsx
@@ -0,0 +1,64 @@
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setisFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  // Função para deixar os icones laranjas quando tiver texto digitado
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    if (inputRef.current?.value) {
+      setisFilled(true);
+    } else {
+      setisFilled(false);
+    }
+  }, []);
+  // Fim
+
+  // Função para deixar os icones e a borda do input laranja quando o usuário clicar
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  // Fim
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
