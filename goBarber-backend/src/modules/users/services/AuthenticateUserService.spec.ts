8c79e7e1004e9ac55ef4c9eaacc5706cbd05350d
Augusto <ahaavila@gmail.com>
2020-08-25 20:41:45 -0300

    Finalizado testes de Autenticação de usuário.

--- goBarber-backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ goBarber-backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -4,7 +4,6 @@ import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-import User from '../infra/typeorm/entities/User';
 
 describe('AuthenticateUserService', () => {
   it('should be able to authenticate', async () => {
@@ -22,7 +21,8 @@ describe('AuthenticateUserService', () => {
       fakeHashProvider,
     );
 
-    await createUser.execute({
+    // criamos um novo user
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -36,6 +36,57 @@ describe('AuthenticateUserService', () => {
 
     // Faço os testes e digo que precisa retornar uma propriedade token e uma user
     expect(response).toHaveProperty('token');
-    expect(response.user).toEqual(User);
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    // passamos para o service o fake repository
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    // criamos nosso service
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    // Faço os testes e digo que precisa retornar um erro, pois não tem 1 usuario criado
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with a wrong password', async () => {
+    // passamos para o service o fake repository
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    // criamos nosso service
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    // criamos um novo user
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    // Faço os testes e digo que precisa retornar um erro, pois a senha ta errada
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
